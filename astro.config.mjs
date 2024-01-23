9abbebbd91f7f5593412baf7815ef52faffc4f43
Simon Twogood <simon.twogood@gmail.com>
2024-01-23 08:44:32 -0700

    Updated landing page with content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react({ experimentalReactChildren: true }), tailwind({ applyBaseStyles: false })]
+  integrations: [
+      react({ experimentalReactChildren: true }),
+      tailwind({ applyBaseStyles: false }),
+      icon(),
+  ]
 });
