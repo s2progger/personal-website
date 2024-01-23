118417ab4d0d6f3cee4f3ffd6d8b298ba81642e0
Simon Twogood <simon.twogood@gmail.com>
2024-01-23 16:25:37 -0700

    Resume content in but needs lots of styling work

--- app/components/TopNav.tsx
+++ app/components/TopNav.tsx
@@ -1,18 +1,21 @@
-import {Button} from "~/components/ui/button"
-import { Icon } from '@iconify/react'
+import {Button} from "~/components/ui/button";
+import { Icon } from '@iconify/react';
 import {Link} from "@remix-run/react";
 import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons"
 
 export default function TopNav() {
   return (
-    <header>
-      <nav className="">
+    <header className="print:hidden">
+      <nav>
         <div className="flex flex-wrap justify-evenly md:justify-between justify-items-stretch items-center gap-2">
           <Link to="/" className="flex items-center left space-x-3 rtl:space-x-reverse">
             <Icon icon="ph:wave-triangle-duotone" className="text-2xl" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Simon Twogood</span>
           </Link>
           <div className="flex items-center gap-1">
+            <Button variant="outline" asChild>
+              <Link to="/resume">Resume</Link>
+            </Button>
             <Button variant="outline" asChild>
               <a href="https://www.linkedin.com/in/simon-twogood" aria-label="LinkedIn Profile"><LinkedInLogoIcon /></a>
             </Button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,6 +53,30 @@ const config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      fontSize: {
+        sm2: "0.9375rem", // 15px label
+      },
+      maxWidth: {
+        "letter": "66.40625rem",
+        "a4": "64.609375rem"
+      },
+      height: {
+        "letter": "85.9375rem",
+        "letter-col": "71.625rem",
+        "letter-col-full": "77.9375rem",
+        "a4": "91.350883rem",
+        "a4-col": "77.038383rem",
+        "a4-col-full": "83.350883rem",
+      },
+      spacing: {
+        "0.5": "2px", // 2px
+        "1.5": "0.375rem", // 6px
+        "1.6": "0.4375rem", // 7px
+        "2.1": "0.5625rem", // 9px
+        "2.5": "10px", // 10px
+        "3.2": "0.8125rem", // 16px
+        "4.5": "1.125rem", // 8px
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
@@ -73,6 +97,14 @@ const config = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
+  },
+  screens: {
+    print: { raw: "print" },
+    xsm: "500px",
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config
