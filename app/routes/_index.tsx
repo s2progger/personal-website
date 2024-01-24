cb8ee3849974c62fdbdde90865e26890ce4f46be
Simon Twogood <simon.twogood@gmail.com>
2024-01-23 21:23:30 -0700

    Fixed lint error

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -15,7 +15,7 @@ export default function Index() {
           Hi there! 👋
         </h1>
         <h2 className="text-3xl md:text-5xl font-bold">
-          My name is <span className="whitespace-nowrap bg-gradient-to-r from-purple-600 via-pink-600 to-blue-600 bg-clip-text text-transparent">Simon</span> and I'm an engineering manager and full stack developer with a passion for building great products.
+          My name is <span className="whitespace-nowrap bg-gradient-to-r from-purple-600 via-pink-600 to-blue-600 bg-clip-text text-transparent">Simon</span> and I&rsquo;m an engineering manager and full stack developer with a passion for building great products.
         </h2>
       </div>
       <div className="md:basis-1/3">
