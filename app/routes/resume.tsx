a84b98a3e4dbd042cfa6e17feed9a2442ea934fd
Simon Twogood <simon.twogood@gmail.com>
2024-01-24 17:41:54 -0700

    Better print formatting

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -19,12 +19,22 @@ export default function Resume () {
   return (
     <main className="print:bg-paper rounded-lg hyphens-manual">
       {/* Page */}
-      <div className="mx-auto page max-w-2xl print:max-w-letter md:max-w-letter md:h-letter p-10 lg:p-16">
+      <div className="mx-auto page max-w-2xl print:max-w-letter md:max-w-letter md:h-letter p-10 lg:p-16 print:p-5">
         <header className="flex align-middle items-center mb-8 md:mb-11">
-          <div className="initials-container text-neutral leading-none bg-space-cadet-250 font-bold mr-3">
+          <div className="print:hidden initials-container leading-none bg-space-cadet-250 font-bold mr-3">
             <div className="initial text-center">S</div>
             <div className="text-center initial">2</div>
           </div>
+          <svg width="1em" height="1em" viewBox="0 0 256 256" className="hidden print:block mr-2 size-8" data-icon="ph:wave-triangle-duotone">
+            <symbol id="ai:ph:wave-triangle-duotone">
+              <g fill="currentColor">
+                <path d="m76 56l52 72H24Zm156 72H128l52 72Z" opacity=".2"/>
+                <path
+                  d="m238.48 132.68l-52 72a8 8 0 0 1-13 0L76 69.66l-45.51 63a8 8 0 1 1-13-9.36l52-72a8 8 0 0 1 13 0l97.51 135l45.51-63a8 8 0 1 1 13 9.36Z"/>
+              </g>
+            </symbol>
+            <use xlinkHref="#ai:ph:wave-triangle-duotone"></use>
+          </svg>
           <h1 className="text-3xl font-semibold">Simon Twogood</h1>
         </header>
         {/* end Name */}
