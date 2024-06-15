f1c5401bfb7678df1d2b591a7715b98a2ca19e3f
Simon Twogood <simon.twogood@gmail.com>
2024-06-14 21:40:03 -0600

    Updated favicons for all platforms

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+// noinspection HtmlRequiredTitleElement
+
 import { LinksFunction } from "@remix-run/cloudflare";
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import tailwind from "~/styles/tailwind.css?url";
@@ -8,6 +10,10 @@ import React from "react";
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: tailwind },
   { rel: "stylesheet", href: fonts },
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+  { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+  { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+  { rel: "manifest", href: "/site.webmanifest" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
