43749664dfd839375ef5a58e1f264d8dcf8ee140
Simon Twogood <simon.twogood@gmail.com>
2024-12-23 13:32:54 -0700

    Added robots.txt and sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -3,9 +3,12 @@ import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://simontwogood.dev",
+  integrations: [tailwind(), sitemap()],
   env: {
     schema: {
       GITHUB_PAT: envField.string({ context: "server", access: "secret" }),
