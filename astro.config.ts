fe0a9c1609ac5e3efb191e84dcf13e598afac44b
Simon Twogood <simon.twogood@gmail.com>
2024-12-23 03:22:15 +0000

    Add GitHub projects component and configure environment variable for GitHub PAT

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  env: {
+    schema: {
+      GITHUB_PAT: envField.string({ context: "server", access: "secret" }),
+    },
+  },
 });
